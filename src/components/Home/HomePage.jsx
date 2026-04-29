50bdc94c6a25bd9036ecd79f468c35d6447d1ce2
Marina Kilbas <margog1982@gmail.com>
2026-04-29 22:32:21 +0300

    добавлено возможность удалять todo и отобрадать сделанное todo

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,40 +1,55 @@
-import React, {useState}  from 'react';
-import  Typography  from '@mui/material/Typography';
+import React, { useState } from 'react';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import TodoItem from './TodoItem';
 import AddTodo from './AddTodo';
 
 
-  const todo ={
-    _id: '234jhg',
-    title: "Подстричься",
-    description: "Сходить в парихмахерскую",
-    isDone: true,
-  } 
+const todo = {
+  _id: '234jhg',
+  title: "Подстричься",
+  description: "Сходить в парихмахерскую",
+  isDone: true,
+}
+
 
 function HomePage(props) {
-const [todos, setTodos] = useState([todo])
+  const [todos, setTodos] = useState([todo])
 
-const handleAddTodo = (title, description) => {
-  const newTodo ={
-     _id: Date.now().toString(),
-    title: title,
-    description: description,
-    isDone: false,
+  const handleAddTodo = (title, description) => {
+    const newTodo = {
+      _id: Date.now().toString(),
+      title: title,
+      description: description,
+      isDone: false,
+    }
+    setTodos([newTodo, ...todos])
   }
-  setTodos([newTodo, ...todos])
-}
+
+  const handleDeleteTodo = (_id) => {
+    setTodos(todos.filter((item) => { return item._id !== _id}))
+  }
+
+   const handleDoneTodo = (_id) => {
+    setTodos(todos.map((item) => { 
+      return item._id === _id ?
+    {...item, isDone: !item.isDone} :
+      item
+    }))
+  }
+
   return (
     <div>
       <Typography>{props.username}</Typography>
-      <AddTodo addTodo = {handleAddTodo} />
-     
+      <AddTodo addTodo={handleAddTodo} />
+
       {
         todos.map((item) => {
-          return  <TodoItem  key ={item._id} todo={item} />
+          return <TodoItem key={item._id} todo={item} handleDeleteTodo={handleDeleteTodo} 
+          handleDoneTodo={handleDoneTodo}/>
         })
       }
-    
+
     </div>
   )
 }
